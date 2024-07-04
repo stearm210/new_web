be0a6be657d9b38e5cf5eb2f8e4a5e6b23d93a42
stearm210 <pyzxwoo@163.com>
2024-07-04 10:48:32 +0800

    修改

--- springboot-web-quickstart/src/main/java/com/itheima/SpringbootWebQuickstartApplication.java
+++ springboot-web-quickstart/src/main/java/com/itheima/SpringbootWebQuickstartApplication.java
@@ -3,6 +3,7 @@ package com.itheima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//这是一个启动类
 @SpringBootApplication
 public class SpringbootWebQuickstartApplication {
 
--- springboot-web-quickstart/src/main/java/com/itheima/controller/HelloController.java
+++ springboot-web-quickstart/src/main/java/com/itheima/controller/HelloController.java
@@ -1,5 +1,15 @@
 package com.itheima.controller;
 
+import jdk.jfr.Registered;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 //请求处理类
-public class HelloController {
+@RestController
+public class HelloController{
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("hello world");
+        return "hello world";
+    }
 }
