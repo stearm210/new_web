f1742238195124fd091db646cba47d8133a63154
stearm210 <pyzxwoo@163.com>
2024-07-15 09:28:11 +0800

    修改

---
+++ springboot-mybatis-quickstart/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,71 @@
+package com.itheima.pojo;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Short age;
+    private Short gender;
+    private String phone;
+
+    public User() {
+    }
+
+    public User(Integer id, String phone, Short gender, Short age, String name) {
+        this.id = id;
+        this.phone = phone;
+        this.gender = gender;
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Short getGender() {
+        return gender;
+    }
+
+    public void setGender(Short gender) {
+        this.gender = gender;
+    }
+
+    public Short getAge() {
+        return age;
+    }
+
+    public void setAge(Short age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
