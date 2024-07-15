dc256cea0e7b4077e436e02da559f0f66d111989
stearm210 <pyzxwoo@163.com>
2024-07-15 17:09:08 +0800

    修改

--- springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
+++ springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -4,5 +4,8 @@ import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface EmpMapper {
-
+    //根据ID删除数据
+    //这里使用注解
+    @Delete("delete from emp where id = #{id}")
+    public void delete(Integer id);
 }
--- springboot-mybatis-crud/src/test/java/com/itheima/SpringbootMybatisCrudApplicationTests.java
+++ springboot-mybatis-crud/src/test/java/com/itheima/SpringbootMybatisCrudApplicationTests.java
@@ -1,13 +1,18 @@
 package com.itheima;
 
+import com.itheima.mapper.EmpMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootMybatisCrudApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired//用于注入注解
+    private EmpMapper empMapper;
 
+    @Test
+    public void testDelete(){
+        empMapper.delete(17);
+    }
 }
