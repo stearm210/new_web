6e5a6876ea0f79936e68b4749f75f4e72866ae25
stearm210 <pyzxwoo@163.com>
2024-07-16 11:11:51 +0800

    修改

--- springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
+++ springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -2,14 +2,25 @@ package com.itheima.mapper;
 
 import org.apache.ibatis.annotations.*;
 
+//这个mapper包中主要是用于编写SQL的操作代码的
+//接口本质上是一个实现类
+//这里用了一个mapper注解，并且定义了一个接口类empmapper进行sql语句的编写和操作
 @Mapper
 public interface EmpMapper {
     //根据ID删除数据
     //这里使用注解
+    //这里的注解后面跟上的是sql语句
+
+    //使用#{id},主要是进行预编译操作问题
+    //使用#{id}，id是数据库中的主键信息，主要是用于sql语句的删除操作
+
     @Delete("delete from emp where id = #{id}")
     //public void delete(Integer id);
 
     //这里的delete的返回值表示此次操作影响到的数据库中的记录数
     //比方说这次操作到底删除了几个记录
     public int delete(Integer id);
+
+
+    //新增员工操作
 }
--- springboot-mybatis-crud/src/main/java/com/itheima/pojo/Emp.java
+++ springboot-mybatis-crud/src/main/java/com/itheima/pojo/Emp.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+//这个pojo主要是存放数据库中各个属性的信息(属性类型、属性名称)
 //这里使用注解使得get、set、有参无参构造可以进行省略
 
 @Data
