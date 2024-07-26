6b74afed4c552bdea7715612e768097017ebf675
stearm210 <pyzxwoo@163.com>
2024-07-26 16:36:55 +0800

    修改

--- springboot-aop-quickstart/src/main/java/com/itheima/aop/MyAspect1.java
+++ springboot-aop-quickstart/src/main/java/com/itheima/aop/MyAspect1.java
@@ -10,13 +10,16 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class MyAspect1 {
 
+    //定义一个共同路径
     @Pointcut("execution(* com.itheima.service.impl.DeptServiceImpl.*(..))")
     public void pt(){}
 
+    //原始方法执行之前执行对应的操作
     @Before("pt()")
     public void before(){
         log.info("before ...");
     }
+
 
     @Around("pt()")
     public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
@@ -25,20 +28,24 @@ public class MyAspect1 {
         //调用目标对象的原始方法执行
         Object result = proceedingJoinPoint.proceed();
 
+        //如果原始方法出现异常，则后面的代码不会运行
         log.info("around after ...");
-        return result;
+        return result;//返回对应的时间
     }
 
+    //原始方法运行之后执行对应的操作
     @After("pt()")
     public void after(){
         log.info("after ...");
     }
 
+    //原始方法正常返回执行对应的操作
     @AfterReturning("pt()")
     public void afterReturning(){
         log.info("afterReturning ...");
     }
 
+    //原始方法出现异常时返回
     @AfterThrowing("pt()")
     public void afterThrowing(){
         log.info("afterThrowing ...");
