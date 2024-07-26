027123af04afc8663b31be6ad763476c4582ba2c
stearm210 <pyzxwoo@163.com>
2024-07-26 11:13:02 +0800

    修改

---
+++ springboot-aop-quickstart/src/main/java/com/itheima/aop/TimeAspect.java
@@ -0,0 +1,4 @@
+package com.itheima.aop;
+
+public class TimeAspect {
+}
--- tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
+++ tlias-web-management/src/main/java/com/itheima/mapper/DeptMapper.java
@@ -2,9 +2,8 @@ package com.itheima.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-/**
- * 部门管理
- */
+/*部门管理
+* */
 @Mapper
 public interface DeptMapper {
 }
