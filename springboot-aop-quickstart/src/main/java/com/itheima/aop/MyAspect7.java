6cc595dd0af521223a0feec4fa665bc61970f9ef
stearm210 <pyzxwoo@163.com>
2024-08-10 08:46:58 +0800

    修改

--- springboot-aop-quickstart/src/main/java/com/itheima/aop/MyAspect7.java
+++ springboot-aop-quickstart/src/main/java/com/itheima/aop/MyAspect7.java
@@ -15,7 +15,7 @@ public class MyAspect7 {
 	//匹配DeptServiceImpl中的 list() 和 delete(Integer id)方法
 	//@Pointcut("execution(* com.itheima.service.DeptService.list()) || execution(* com.itheima.service.DeptService.delete(java.lang.Integer))")
 	//
-	//annotation表达式主要用于自定义注解方式
+	//annotation表达式主要用于自定义注解方式,只要是service层的实现类中的实现方法上加上了MyLog注解，就可以使用到这个注解对应的操作
 	@Pointcut("@annotation(com.itheima.aop.MyLog)")
 	private void pt(){
 
